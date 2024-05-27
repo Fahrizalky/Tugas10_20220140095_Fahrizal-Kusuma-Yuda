12b8d4a98864cdab68b575ad71954d29f5e2c85d
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2024-05-27 10:11:28 +0700

    Tugas Pertemuan 12

---
+++ script.js
@@ -0,0 +1,25 @@
+function kirimdata() {
+    var name = document.getElementById("nama").value;
+    var nim = document.getElementById("nim").value;
+    var peminatan = document.querySelector('input[name="peminatan"]:checked').value;
+    var alamat = document.getElementById("alamat").value;
+    var tanggal = document.getElementById("tanggal").value;
+    var angkatan = document.getElementById("angkatan").value;
+
+    alert(
+        "Nama: " + name +
+        "\nNIM: " + nim +
+        "\nPeminatan: " + peminatan +
+        "\nAlamat: " + alamat +
+        "\nTanggal: " + tanggal +
+        "\nAngkatan: " + angkatan
+    );
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var tanggalInput = document.getElementById('tanggal');
+    flatpickr(tanggalInput, {
+        enableTime: false,
+        dateFormat: 'd-m-y',
+    });
+});
